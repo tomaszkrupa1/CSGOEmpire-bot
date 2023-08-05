aa5d3cf649f728ff4d833416252eafe5167affb1
tomaszkrupa1 <tomaszkrupa1999@gmail.com>
2023-08-05 17:49:49 +0100

    Webpage element analysis

---
+++ script.js
@@ -0,0 +1,14 @@
+///  WEBPAGE ELEMENTS  \\\
+
+// Bet input field - document.getElementsByClassName("relative z-10 h-full w-full bg-transparent")[0].value
+// Countdown - document.getElementsByClassName("font-numeric text-2xl font-bold")[0].textContent
+
+// CT Button - document.getElementsByClassName("bet-btn")[0]
+// Dice Button - document.getElementsByClassName("bet-btn")[1]
+// T Button - document.getElementsByClassName("bet-btn")[2]
+
+// Previous Roll - document.getElementsByClassName("previous-rolls-item")[19].children[0].className
+// CT = coin-ct ml-1 inline-block h-24 w-24 rounded-full
+// Dice = coin-bonus ml-1 inline-block h-24 w-24 rounded-full
+// T = coin-t ml-1 inline-block h-24 w-24 rounded-full
+
